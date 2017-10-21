c88d962faea37dbb817a748360b3c7014a1066df
x8086 <43070810@qq.com>
2017-10-21 12:04:56 +0800

    iframe

---
+++ cors/iframe/origin_a/a_main.js
@@ -0,0 +1,25 @@
+var http=require('http')
+var fs=require('fs')
+var url=require('url')
+
+var port=process.env.port || 8888;
+
+var server=http.createServer(function(request,response){
+    var temp=url.parse(request.url,true)
+    var path=temp.pathname
+    var query=temp.query
+    console.log(`请求原始内容是:${request}`)
+    console.log(`请求内容格式化后:${path}`)
+    console.log(query.index)
+    console.log(query.length)
+
+    if(path==='/a_index.html'){
+        response.setHeader('Content-Type','text/html;charset="utf-8"')
+        var stringIndex=fs.readFileSync('./a_index.html','utf-8')
+        response.end(stringIndex)
+    }       
+})
+
+server.listen(port)
+console.log(`监听 ${port} 成功,请打开http://localhost:${port}`)
+console.log('this is a html')
